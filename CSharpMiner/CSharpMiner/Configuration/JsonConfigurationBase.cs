77aaca2c218d763240675547a43123d1eaf51163
manzilla <the_best_flash@hotmail.com>
2014-06-15 17:29:50 -0700

    Added configuration loading and starting to add TCP connection code.

---
+++ CSharpMiner/CSharpMiner/Configuration/JsonConfigurationBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using CSharpMiner.Stratum;
+
+namespace CSharpMiner.Configuration
+{
+    [DataContract]
+    public class JsonConfigurationBase : IConfiguration
+    {
+        private static DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(JsonConfigurationBase));
+
+        [IgnoreDataMember]
+        public virtual DataContractJsonSerializer Serializer { get { return JsonConfigurationBase.serializer; } }
+
+        [DataMember(Name = "pools")]
+        public Pool[] Pools { get; set; }
+
+        public JsonConfigurationBase()
+        {
+            Pools = new Pool[0];
+        }
+
+        public JsonConfigurationBase(Pool[] pools)
+        {
+            Pools = pools;
+        }
+
+        public void Load(System.IO.Stream stream)
+        {
+            this.Load((IConfiguration)this.Serializer.ReadObject(stream));
+        }
+
+        public void Save(System.IO.Stream stream)
+        {
+            this.Serializer.WriteObject(stream, this);
+        }
+
+        protected virtual void Load(IConfiguration config)
+        {
+            Pools = config.Pools;
+        }
+    }
+}
--- CSharpMiner/CSharpMiner/Program.cs
+++ CSharpMiner/CSharpMiner/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using CSharpMiner.Configuration;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +14,39 @@ namespace CSharpMiner
     {
         static void Main(string[] args)
         {
+            if(args.Length != 1)
+            {
+                WriteUsage();
+                return;
+            }
+
+            JsonConfigurationBase config = new JsonConfigurationBase();
+
+            try
+            {
+                using (var inputFile = File.OpenRead(args[0]))
+                {
+                    config.Load(inputFile);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Configuration file not found. {0}", args[0]);
+                return;
+            }
+            catch(SerializationException e)
+            {
+                Console.WriteLine("There was an error loading the configuration file:");
+                Console.WriteLine(e.InnerException.Message);
+                return;
+            }
+
+            TcpClient client = new TcpClient("", 0);
+        }
+
+        static void WriteUsage()
+        {
+            Console.WriteLine("CSharpMiner.exe <Configuration File Path>");
         }
     }
 }
