cb3f770d60803e9aa6f44aa4b46c4f12cdd10172
manzilla <the_best_flash@hotmail.com>
2014-06-21 01:32:20 -0700

    Adding missing reference

--- CSharpMiner/CSharpMiner/MonoComplie.sh
+++ CSharpMiner/CSharpMiner/MonoComplie.sh
@@ -1,1 +1,1 @@
-﻿mcs -optimize+ -recurse:*.cs -out:CSharpMiner.exe -r:System.Data.DataSetExtensions,System.Security
+﻿mcs -optimize+ -recurse:*.cs -out:CSharpMiner.exe -r:System.Data.DataSetExtensions,System.Security,System.Runtime.Serialization
