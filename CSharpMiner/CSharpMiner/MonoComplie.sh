bb9a3d36b4816b37a43a534800cb9b9141247674
manzilla <the_best_flash@hotmail.com>
2014-06-21 01:43:18 -0700

    Adding more references

--- CSharpMiner/CSharpMiner/MonoComplie.sh
+++ CSharpMiner/CSharpMiner/MonoComplie.sh
@@ -1,1 +1,1 @@
-﻿mcs -optimize+ -recurse:*.cs -out:CSharpMiner.exe -r:System.Data.DataSetExtensions,System.Security,System.Runtime.Serialization
+﻿mcs -optimize+ -recurse:*.cs -out:CSharpMiner.exe -r:System,System.Core,System.Data,System.Data.DataSetExtensions,System.Security,System.Runtime.Serialization,System.Xml,System.Xml.Linq
