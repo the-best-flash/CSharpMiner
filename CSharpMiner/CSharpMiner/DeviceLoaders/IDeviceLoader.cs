3b221b17aa5e9ea247acd120b0fdd0161920e06a
manzilla <the_best_flash@hotmail.com>
2014-06-22 02:08:35 -0700

    Adding copyright information.

--- CSharpMiner/CSharpMiner/DeviceLoaders/IDeviceLoader.cs
+++ CSharpMiner/CSharpMiner/DeviceLoaders/IDeviceLoader.cs
@@ -1,4 +1,20 @@
-﻿using MiningDevice;
+﻿/*  Copyright (C) 2014 Colton Manville
+    This file is part of CSharpMiner.
+
+    CSharpMiner is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CSharpMiner is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CSharpMiner.  If not, see <http://www.gnu.org/licenses/>.*/
+
+using MiningDevice;
 using System;
 using System.Collections.Generic;
 using System.Linq;
