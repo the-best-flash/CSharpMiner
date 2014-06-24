520fb6cf3610fae658e1ee695cea5f79d785d3c8
manzilla <pi@scrypt-miner.(none)>
2014-06-24 00:43:15 -0700

    Updating build files to delete any old files left. Adding debug build file.

--- MonoCompile.sh
+++ MonoCompile.sh
@@ -1,4 +1,5 @@
 mkdir bin
+rm bin/*
 mcs -optimize+ -recurse:CSharpMiner/CSharpMiner/*.cs -out:CSharpMinerLib.dll -target:library -r:System,System.Core,System.Data,System.Data.DataSetExtensions,System.Security,System.Runtime.Serialization,System.Xml,System.Xml.Linq
 mcs -optimize+ -recurse:CSharpMiner/StratumPlugin/*.cs -out:bin/StratumPlugin.dll -target:library -r:System,System.Core,System.Data,System.Data.DataSetExtensions,System.Runtime.Serialization,CSharpMinerLib.dll
 mcs -optimize+ -recurse:CSharpMiner/ZeusMinerGen1Plugin/*.cs -out:bin/ZeusMinerGen1Plugin.dll -target:library -r:System,System.Core,System.Data,System.Data.DataSetExtensions,System.Runtime.Serialization,CSharpMinerLib.dll
