3c0624c693ff19df426f4b9d4983383cbbd45872
the-best-flash <the_best_flash@hotmail.com>
2014-07-02 03:04:00 -0700

    Updating mono build scripts.

--- DebugMonoCompile.sh
+++ DebugMonoCompile.sh
@@ -3,6 +3,7 @@ rm bin/*
 mcs -optimize- -debug+ -d:DEBUG -recurse:CSharpMiner/CSharpMiner/*.cs -out:CSharpMinerLib.dll -target:library -r:System,System.Core,System.Data,System.Data.DataSetExtensions,System.Security,System.Runtime.Serialization,System.Xml,System.Xml.Linq
 mcs -optimize- -debug+ -d:DEBUG -recurse:CSharpMiner/StratumPlugin/*.cs -out:bin/StratumPlugin.dll -target:library -r:System,System.Core,System.Data,System.Data.DataSetExtensions,System.Runtime.Serialization,CSharpMinerLib.dll
 mcs -optimize- -debug+ -d:DEBUG -recurse:CSharpMiner/ZeusMinerGen1Plugin/*.cs -out:bin/ZeusMinerGen1Plugin.dll -target:library -r:System,System.Core,System.Data,System.Data.DataSetExtensions,System.Runtime.Serialization,CSharpMinerLib.dll
+mcs -optimize- -debug+ -d:DEBUG -recurse:CSharpMiner/GridseedGen1Plugin/*.cs -out:bin/GridseedPlugin.dll -target:library -r:System,System.Core,System.Data,System.Data.DataSetExtensions,System.Runtime.Serialization,CSharpMinerLib.dll
 mcs -optimize- -debug+ -d:DEBUG -target:exe CSharpMiner/CSharpMinerProgram/Program.cs -out:CSharpMiner.exe -r:CSharpMinerLib.dll
 
 
